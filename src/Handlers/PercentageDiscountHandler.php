384272a2b69428913949288329c595fcd0e032d7
Ahmed Ghorab <aghorab@outlook.com>
2023-08-13 07:19:13 +0300

    fix error in phpstan

--- src/Handlers/PercentageDiscountHandler.php
+++ src/Handlers/PercentageDiscountHandler.php
@@ -2,10 +2,6 @@
 
 namespace AGhorab\LaravelPromocode\Handlers;
 
-use AGhorab\LaravelPromocode\Traits\ApplyPromocodeHandler;
-use Exception;
-use Illuminate\Database\Eloquent\Model;
-
 class PercentageDiscountHandler extends BaseDiscountHandler
 {
     public function __construct(private readonly float $percentage)
@@ -15,32 +11,5 @@ class PercentageDiscountHandler extends BaseDiscountHandler
     public function calculate(float $value): float
     {
         return (100 - $this->percentage) * $value / 100;
-    }
-
-    public function handle(Model $model)
-    {
-        if (! in_array(ApplyPromocodeHandler::class, class_uses_recursive($model))) {
-            throw new Exception("Model doesn't have Apply Promocode Handler");
-        }
-
-        /** @var ApplyPromocodeHandler */
-        $handler = $model;
-        foreach ($handler->getDiscountableItems() as $attribute => [$discount, $original]) {
-            $originalAmount = $model->{$attribute};
-
-            $discountAmount = ($originalAmount * $this->percentage) / 100;
-
-            if ($discount) {
-                $model->{$discount} = $discountAmount;
-            }
-
-            if ($original) {
-                $model->{$original} = $originalAmount;
-            }
-
-            $model->{$attribute} = $originalAmount - $discountAmount;
-        }
-
-        $model->save();
     }
 }
